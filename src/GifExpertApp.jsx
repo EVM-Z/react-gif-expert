8d46b652cb3ba3a765a13ac3665c833c7ea4c261
EliseoVM <elyzeo.vm@gmail.com>
2022-07-01 23:39:38 -0600

    Agregar una nueva categoria

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -4,12 +4,26 @@ export const GifExpertApp = () => {
 
   const [categories, setCategories] = useState([ 'One Puch', 'Dragon Ball' ]);
   
-  console.log(categories);
+  // console.log(categories);
+  const onAddCategory = () => {
+    
+    // categories.push('Naruto');
+    // ...categories hace una copia del arreglo
+
+    // Primera forma de agregar un elemento al arreeglo
+    // setCategories([ ...categories, 'Naruto' ]);
+
+    // Segunda forma
+    setCategories( categories => [ ...categories, 'Sasuke' ]);
+
+  }
 
   return (
     
     <>
         <h1>GifExpertApp</h1>
+
+        <button onClick={ onAddCategory }>Agregar</button>
 
         <ol>
           {
